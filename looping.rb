63c9625aec35d24798ffeb129de6db4c11ca195a
Celeste Gerard <celeste.gerard@gmail.com>
2018-02-27 22:31:52 +0000

    automatically backed up by learn

--- looping.rb
+++ looping.rb
@@ -2,12 +2,7 @@ def looping
 	counter = 0
  
 loop do 
-  counter += 1
-  puts "Iteration #{counter} of the loop"
-  if counter >= 10 
-    break
-  end
-end
+  puts "Wingardium Leviosa"
 end
 
 #call your method here
